b13ad01dc0167d89c8e4c4ef7c63573ac11471c1
Elshan hasanov <elsen.hasanovv@gmail.com>
2025-07-20 11:55:29 +0400

    Mesaj

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
         InfoPrinter printer = new InfoPrinter();
+        Scanner scanner = new Scanner(System.in);
 
-        printer.printInfo();
-        printer.printInfo("Elsen");
-        printer.printInfo("Elsen", 28);
-        printer.printInfo("Elsen", 28, "Bakı");
+        String name;
+        int age;
+        String university;
+
+        System.out.print("Adı daxil edin: ");
+        name = scanner.nextLine();
+        printer.printInfo(name);
+
+        System.out.print("Adı daxil edin: ");
+        name = scanner.nextLine();
+        System.out.print("Yaşı daxil edin: ");
+        age = scanner.nextInt();
+        scanner.nextLine();
+        printer.printInfo(name, age);
+
+        System.out.print("Adı daxil edin: ");
+        name = scanner.nextLine();
+        System.out.print("Yaşı daxil edin: ");
+        age = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("Universiteti daxil edin: ");
+        university = scanner.nextLine();
+        printer.printInfo(name, age, university);
+
+        scanner.close();
     }
 }
